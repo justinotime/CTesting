1ccaf0fd31fe5b7b97cfe5d585d791522918c81f
justinotime <jjnatividad1209@gmail.com>
2022-10-01 11:33:22 -0700

    Binary search

---
+++ Algorithms/BinarySearch/CLangWin/Main.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int binarySearch() {
+    int mid = 0;
+    int low = 0;
+    int high;
+    
+    return -1;
+}
+
+int userInputValue()
+{
+    int userValue;
+    cout << "Enter a value: ";
+    cin >> userValue;
+    return userValue;
+}
+
+int main() {
+    int array [8] = {2,5,6,14,16,24,32,63}; 
+    int value;
+    cout << ("Number Array: ");
+    // Checking if Array works
+    for (int i = 0; i < 8; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    // Entering values
+    value = userInputValue();
+    // cout << value; (Just to check) 
+
+    // Comparison
+    
+
+    return 0;
+}
---
+++ Algorithms/BinarySearch/Python/main.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    randarray = {2,4,7,10,11,32,45,87}
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
