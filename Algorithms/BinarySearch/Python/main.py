467aebda04477d21332395fec5fae817fe588c72
justinotime <jjnatividad1209@gmail.com>
2022-10-04 00:02:53 -0700

    12:02 | 10/4/2022

--- Algorithms/BinarySearch/Python/main.py
+++ Algorithms/BinarySearch/Python/main.py
@@ -1,8 +1,60 @@
 
 
-def main():
-    randarray = {2,4,7,10,11,32,45,87}
+from array import array
+from asyncio.windows_events import NULL
 
+
+def initArray(*userPlay):
+    userPlay = 0 
+    while userPlay != 1:
+        userYesNo = input("Do you want to create an array? Y/N only: ") 
+        if (userYesNo == 'Y'):
+            userPlay += 1
+            break
+        elif (userYesNo == 'N'):
+            userPlay = 0
+            break
+        else:
+            print("Woops.. try again")
+            continue
+    
+def putArray(userPlay, randArray):
+    randArray = []
+    while (userPlay == 1):
+        for i in range(6):
+            userValArray = input("Enter values for Array: ")
+            randArray.append(userValArray)
+            print(randArray)
+        else:
+            print("This is finished")
+        userPlay -= 1
+
+def main():
+    Array = []
+    usersPlay = NULL
+    usersValues = 0
+    # while userPlay != 1:
+    #     userYesNo = input("Do you want to create an array? Y/N only: ") 
+    #     if (userYesNo == 'Y'):
+    #         userPlay += 1
+    #         break
+    #     elif (userYesNo == 'N'):
+    #         userPlay = 0
+    #         break
+    #     else:
+    #         print("Woops.. try again")
+    #         continue   
+    initArray(usersPlay)
+    putArray(usersPlay, array)
+    # while (userPlay == 1):
+    #     for i in range(6):
+    #         userValArray = input("Enter values for Array: ")
+    #         randArray.append(userValArray)
+    #         print(randArray)
+    #     else:
+    #         print("This is finished")
+    #     userPlay -= 1
+    
 if __name__ == "__main__":
     main()
 
