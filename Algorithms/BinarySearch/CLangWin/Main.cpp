48101efebd546e77ad101599536ecc46301e7355
justinotime <jjnatividad1209@gmail.com>
2022-10-04 13:01:30 -0700

    Mac | 10/04/2022 | 1:01 PM

---
+++ Algorithms/BinarySearch/CLangWin/Main.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int binarySearch() {
+    int mid = 0;
+    int low = 0;
+    int high;
+    
+    return -1;
+}
+
+int userInputValue()
+{
+    int userValue;
+    cout << "Enter a value: ";
+    cin >> userValue;
+    return userValue;
+}
+
+int main() {
+    int array [8] = {2,5,6,14,16,24,32,63};
+    int *pointArray = array; 
+    int value;
+    cout << ("Number Array: ");
+    
+    // Checking if Array works
+    for (int i = 0; i < 8; i++) {
+        cout << *(pointArray + i) << " ";
+    }
+    cout << endl;
+    
+    // Entering values
+    value = userInputValue();
+    // cout << value; (Just to check) 
+
+    // Comparison
+    
+
+    return 0;
+}
