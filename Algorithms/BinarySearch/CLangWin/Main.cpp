35a40ed343d7de14b2881f7260a2ccbb7fe51aeb
justinotime <jjnatividad1209@gmail.com>
2022-10-07 00:35:50 -0700

    Windows

--- Algorithms/BinarySearch/CLangWin/Main.cpp
+++ Algorithms/BinarySearch/CLangWin/Main.cpp
@@ -4,40 +4,57 @@ using namespace std;
 
 
 
-int binarySearch() {
-    int mid = 0;
+int binarySearch(int* values, int valuesSize, int key) {
+    
     int low = 0;
-    int high;
+    int high = valuesSize - 1;
+
+    while (high >= low) {
+        int mid = (high + low) / 2;
+
+        if (values[mid] < key) {
+            low = mid + 1;
+        }
+        else if (values[mid] > key) {
+            high = mid - 1;
+        }
+        else {
+            return mid;
+        }
+    }
     
     return -1;
 }
 
-int userInputValue()
-{
-    int userValue;
-    cout << "Enter a value: ";
-    cin >> userValue;
-    return userValue;
-}
+
 
 int main() {
-    int array [8] = {2,5,6,14,16,24,32,63};
-    int *pointArray = array; 
-    int value;
-    cout << ("Number Array: ");
-    
-    // Checking if Array works
-    for (int i = 0; i < 8; i++) {
-        cout << *(pointArray + i) << " ";
-    }
-    cout << endl;
-    
-    // Entering values
-    value = userInputValue();
-    // cout << value; (Just to check) 
+    int array[] = {2,5,6,14,16,24,32,63};
+    int arraySize = sizeof(array) / sizeof(array[0]);
 
-    // Comparison
+    // for (unsigned int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    // {
+    //     cout << "Number Array: " << array[i] << endl;
+    // }
     
+    // The better methodology to loop through an array
+    cout << "Values: ";
+    for (const int &array : array)
+    {
+        cout << array << " ";
+    }
+
+    cout << "Enter an integer value: ";
+    int key = 0;
+    cin >> key;
+    
+    int keyIndex = binarySearch(array, arraySize, key);
+    if (keyIndex == -1) {
+        cout << key << " was not found." << endl;
+    }
+    else {
+        cout << "Found " << key << " at index " << keyIndex << "." << endl;
+    }
 
     return 0;
 }
---
+++ ProblemSolve/Binary search template function/CLangWin/Searcher.h
@@ -0,0 +1,34 @@
+#ifndef SEARCHER_H
+#define SEARCHER_H
+
+#include "Comparer.h"
+
+template <typename T>
+class Searcher {
+public:
+   // Returns the index of the key in the sorted array, or -1 if the key is not
+   // found
+   static int BinarySearch(T* array, int arraySize, const T& key,
+      Comparer<T>& comparer) {
+      // Your code here (remove placeholder line below)
+      int low = 0;
+      int high = arraySize - 1;
+
+      while (high >= low) {
+         int mid = (high + low) / 2;
+
+         if (array[mid] < key) {
+            low = mid + 1;
+         }
+         else if (array[mid] > key) {
+            high = mid - 1;
+         }
+         else {
+            return mid;
+         }
+      }
+      return -1;
+   }
+};
+
+#endif
